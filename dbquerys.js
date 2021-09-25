60885d00c30d54d4f55ad0a9b3670f5e99fd8654
German Gerardo Guerci <germangguerci@gmail.com>
2021-09-25 20:56:28 -0300

    Mockup data, sample querys.

---
+++ dbquerys.js
@@ -0,0 +1,65 @@
+export const CREATE_USERS_TABLE = `CREATE TABLE IF NOT EXISTS users  (
+user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+full_name TEXT NOT NULL,
+date_of_birth REAL NOT NULL,
+dni TEXT NOT NULL UNIQUE,
+phone TEXT,
+email TEXT NOT NULL,
+username TEXT NOT NULL,
+password TEXT NOT NULL,
+created_at REAL DEFAULT (julianday('now')),
+updated_at REAL
+)`;
+
+export const CREATE_ACCOUNTS_TABLE = `CREATE TABLE IF NOT EXISTS accounts (
+account_id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+user_id INTEGER,
+cbu TEXT NOT NULL,
+balance INTEGER NOT NULL,
+currency TEXT NOT NULL,
+pin INTEGER DEFAULT '0',
+created_at REAL DEFAULT (julianday('now')),
+updated_at REAL,
+FOREIGN KEY(user_id) REFERENCES users(user_id)
+);`;
+
+export const CREATE_TRANSFERS_TABLE = `CREATE TABLE IF NOT EXISTS transfers (
+order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+origin_account INTEGER NOT NULL,
+destiny_account INTEGER NOT NULL,
+value INTEGER NOT NULL,
+created_at REAL DEFAULT (julianday('now')),
+remaining_balance INTEGER NOT NULL,
+FOREIGN KEY(origin_account) REFERENCES accounts(account_id),
+FOREIGN KEY(destiny_account) REFERENCES users(account_id)
+);`;
+
+export const CREATE_DEPOSITS_TABLE = `CREATE TABLE IF NOT EXISTS deposits (
+deposit_id INTEGER PRIMARY KEY,
+account_id INTEGER,
+created_at REAL DEFAULT (julianday('now')),
+expire_date REAL,
+FOREIGN KEY(account_id) REFERENCES accounts(account_id)
+);`;
+
+/* INSERT INTO users (full_name, date_of_birth, dni, phone, email, username, password)
+VALUES ('german gerardo guerci', julianday('1996-10-08'), '40130627', '542257636857', 'germangguerci@gmail.com', 'germangguerci', 'tobogan') ;
+
+INSERT INTO accounts (account_id, user_id, cbu, balance, currency )
+VALUES (
+'204878658',
+'2',
+'01702046600000087865',
+'0',
+'ARS'
+) ;
+
+
+INSERT INTO transfers (origin_account, destiny_account, value, remaining_balance)
+VALUES (
+'1',
+'2',
+'500',
+'-500'
+) ;
+ */
--- repositories/dao.js
+++ repositories/dao.js
@@ -1,25 +1,28 @@
+import {
+  CREATE_USERS_TABLE,
+  CREATE_DEPOSITS_TABLE,
+  CREATE_TRANSFERS_TABLE,
+  CREATE_ACCOUNTS_TABLE,
+} from "../dbquerys";
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("dbtest.sqlite");
+const db = new sqlite3.Database("bankDb.s3db");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 export default class {
   static setupDbForDev() {
     db.serialize(function () {
-      const createUsersTable =
-        "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT, password text)";
-      db.run(createUsersTable);
-      const createItemsTable =
-        "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price NUMERIC)";
-      db.run(createItemsTable);
-      let password = "123";
-
+      db.run(CREATE_USERS_TABLE);
+      db.run(CREATE_ACCOUNTS_TABLE);
+      db.run(CREATE_TRANSFERS_TABLE);
+      db.run(CREATE_DEPOSITS_TABLE);
+      let password = "Azucar1234!";
       bcrypt.hash(password, saltRounds, function (err, hash) {
-        const insertUsers = `INSERT INTO users (username, password) VALUES ('foo', '${hash}'), ('bar', '${hash}');`;
+        const insertUsers = `INSERT INTO users (full_name, date_of_birth, dni, phone, email, username, password)
+        VALUES ('Pedro Picapiedra', julianday('1996-12-12'), '40130620', '542257624787', 'pedropicapiedra@gmail.com', 'pedropicapiedra', '${hash}'),
+        ('Marge Simpson', julianday('1985-10-11'), '30130620', '542257624787', 'pedropicapiedra@gmail.com', 'pedropicapiedra', '${hash}');`;
         db.run(insertUsers);
       });
-      const insertItems = `INSERT INTO items (name, price) VALUES ('book', 12.99), ('t-shirt', 15.99), ('milk', 3.99);`;
-      db.run(insertItems);
     });
     //  db.close();
   }
