ddbb36aa0655072eea13435ff5f4db45251c25d2
German Gerardo Guerci <germangguerci@gmail.com>
2021-09-25 12:29:17 -0300

    Project initialized from the template and update deprecated modules.
    Template based from:
    https://github.com/mwolfhoffman/node-jwt-sqlite-starter
    
    Changes :
    -Removed some warnings.
    -Removed deprecated body-parse / cookie-parser.
    -Db is saved in file instead of memory.

---
+++ app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dao from "./repositories/dao";
+import { authenticated, authMiddleware } from "./controllers/auth.controller";
+import authRoutes from "./routes/auth.routes";
+import itemsRoutes from "./routes/items.routes";
+const session = require("express-session");
+import * as sqlite3 from "sqlite3";
+import sqliteStoreFactory from "express-session-sqlite";
+
+const port = 3000;
+export const app = express();
+
+app.listen(port, () =>
+  console.log(`Authentication example app listening on port ${port}!`)
+);
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(authMiddleware);
+
+app.use(session({ secret: "super secret string" }));
+const SqliteStore = sqliteStoreFactory(session);
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "MAGIC SECRET STRING",
+    store: new SqliteStore({
+      driver: sqlite3.Database,
+      path: "dbtest.sqlite",
+      ttl: 604800000, // 1 week in miliseconds
+    }),
+  })
+);
+
+dao.setupDbForDev();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/items", authenticated, itemsRoutes);
---
+++ repositories/repository.js
@@ -0,0 +1,21 @@
+import dao from "./dao";
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+export default class {
+  static async getAllItems() {
+    return dao.all("SELECT * FROM items", []);
+  }
+
+  static async getItemById(id) {
+    return dao.get("SELECT * FROM items WHERE id = ?", [id]);
+  }
+
+  static async getUserByUsername(username) {
+    return dao.get("SELECT * FROM users WHERE username =?", [username]);
+  }
+
+  static async getUserById(id) {
+    return dao.get("SELECT * FROM users WHERE id = ?", [id]);
+  }
+}
